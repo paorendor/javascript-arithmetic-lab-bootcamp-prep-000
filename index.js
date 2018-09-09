1e92f6350ef89ff8d1deae73d9dea3bb56288c5a
paorendor@gmail.com rendor <paorendor@gmail.com>
2018-09-09 04:45:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,49 @@
+
+
+function beforeEach() 
+{
+  var a = Math.floor(Math.random() * 1000)
+  var b = Math.floor(Math.random() * 1000)
+}
+
+
+function preserveDecimal(n)
+{
+  parseInt(n)
+}
+
+beforeEach()
+
+function add(a,b)
+{
+ return a + b
+}
+
+function subtract(a,b)
+{
+  return a-b
+}
+
+function multiply(a,b)
+{
+  return a*b
+}
+
+function divide(a,b)
+{
+  return a / b
+}
+
+function inc(n)
+{
+  return n + 1
+}
+
+function dec(n)
+{
+  return n - 1
+}
+
+
+
+
